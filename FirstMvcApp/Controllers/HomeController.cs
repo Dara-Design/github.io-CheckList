95940da58a587f633eb038725e4f2374fb9a2587
Miss Dara <fashioninmotion88@gmail.com>
2020-10-06 19:17:55 +0200

    Importing project

---
+++ FirstMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMvcApp.Models;
+using System.Web.Security;
+
+namespace FirstMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string FirstName)
+        {
+            Attendance.AddAttendant(FirstName);
+            TempData["FirstName"] = FirstName;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FirstMvcApp/Models/Attendance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMvcApp.Models
+{
+    public class Attendance
+    {
+        private static List<string> attendants = new List<string>();
+
+        public static void AddAttendant(string attendantName)
+        {
+            if (attendants != null)
+            {
+                attendants.Add(attendantName);
+            }
+        }
+
+        public static List<string> GetAttendants()
+        {
+            return attendants;
+        }
+
+    }
+}
